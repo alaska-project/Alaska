f4a263d9dd4c144b2b8fa0312fe4e318d77c26cf
Simone Sabba <Simone.Sabba@alpitourworld.it>
2019-06-07 11:41:40 +0200

    unit tests

---
+++ src/plugins/Sitecore.Plugins.Alaska.Contents.UnitTests/Adapters/DefaultFieldAdapterTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sitecore.Plugins.Alaska.Contents.Adapters.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Plugins.Alaska.Contents.UnitTests.Adapters
+{
+    [TestClass]
+    public class DefaultFieldAdapterTests
+    {
+        private DefaultFieldAdapter _adapter = new DefaultFieldAdapter();
+
+        [TestMethod]
+        public void ReadStringField()
+        { }
+
+        [TestMethod]
+        public void WriteStringField()
+        { }
+    }
+}
---
+++ src/plugins/Sitecore.Plugins.Alaska.Contents.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Plugins.Alaska.Contents.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sitecore.Plugins.Alaska.Contents.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d84bcf23-3b3b-4807-a566-01ce014df26a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

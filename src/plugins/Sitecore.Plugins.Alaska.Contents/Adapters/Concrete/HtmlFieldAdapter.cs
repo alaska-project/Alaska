e18c6d2a6e02a2fbbee6c20d09cc3b926cdee96c
Simone Sabba <Simone.Sabba@alpitourworld.it>
2019-06-07 23:27:09 +0200

    adapters

---
+++ src/plugins/Sitecore.Plugins.Alaska.Contents/Adapters/Concrete/HtmlFieldAdapter.cs
@@ -0,0 +1,28 @@
+﻿using Alaska.Services.Contents.Domain.Models.Items;
+using Sitecore.Data.Fields;
+using Sitecore.Plugins.Alaska.Contents.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Plugins.Alaska.Contents.Adapters.Concrete
+{
+    public class HtmlFieldAdapter : IFieldAdapter
+    {
+        public ContentItemField AdaptField(Field field)
+        {
+            return new ContentItemField
+            {
+                Type = DefaultFieldTypes.Html,
+                Value = field.Value,
+            };
+        }
+
+        public void UpdateField(ContentItemField value, Field field)
+        {
+            field.Value = (string)value.Value;
+        }
+    }
+}
